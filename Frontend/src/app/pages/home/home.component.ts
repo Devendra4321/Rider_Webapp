5cec884b5572145fc34a758d95452c54a4a23285
Devendra4321 <devendrabhole3369@gmail.com>
2025-01-24 23:30:38 +0530

    Initialize Angular frontend structure and added home page

---
+++ Frontend/src/app/pages/home/home.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import mapboxgl from 'mapbox-gl';
+import { environment } from '../../../environment/environment';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent {
+  map!: mapboxgl.Map;
+
+  ngAfterViewInit(): void {
+    (mapboxgl as any).accessToken = environment.MAP_BOX_ACCESS_TOKEN;
+
+    this.map = new mapboxgl.Map({
+      container: 'map', // ID of the div where map will be rendered
+      style: 'mapbox://styles/mapbox/streets-v11', // Map style
+      center: [77.209, 28.6139], // Longitude, Latitude (Example: New Delhi, India)
+      zoom: 12,
+    });
+
+    // Add zoom and rotation controls
+    this.map.addControl(new mapboxgl.NavigationControl());
+  }
+}
