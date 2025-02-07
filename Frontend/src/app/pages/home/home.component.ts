f1f8e0af399dd2a39a210bced9082aec5f231d00
Devendra4321 <devendrabhole3369@gmail.com>
2025-02-07 21:52:24 +0530

    Refactor home component layout and styles

--- Frontend/src/app/pages/home/home.component.ts
+++ Frontend/src/app/pages/home/home.component.ts
@@ -8,20 +8,4 @@ import { RideSocketService } from '../../services/ride-socket/ride-socket.servic
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {
-  map!: mapboxgl.Map;
-
-  ngAfterViewInit(): void {
-    (mapboxgl as any).accessToken = environment.MAP_BOX_ACCESS_TOKEN;
-
-    this.map = new mapboxgl.Map({
-      container: 'map', // ID of the div where map will be rendered
-      style: 'mapbox://styles/mapbox/streets-v11', // Map style
-      center: [77.209, 28.6139], // Longitude, Latitude (Example: New Delhi, India)
-      zoom: 12,
-    });
-
-    // Add zoom and rotation controls
-    this.map.addControl(new mapboxgl.NavigationControl());
-  }
-}
+export class HomeComponent {}
