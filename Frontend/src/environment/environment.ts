50545b826ae018f23bbf47d9dc276b47eaa9ac94
Devendra4321 <devendrabhole3369@gmail.com>
2025-02-08 16:02:52 +0530

    7 Optimize for deploy build

--- Frontend/src/environment/environment.ts
+++ Frontend/src/environment/environment.ts
@@ -1,18 +1,18 @@
 export const environment = {
   production: true,
 
-  // SOCKET_URL: 'https://riderbackend-5rue.onrender.com/',
-  // API_URL_USER: 'https://riderbackend-5rue.onrender.com/users/',
-  // API_URL_CAPTAIN: 'https://riderbackend-5rue.onrender.com/captains/',
-  // API_URL_MAP: 'https://riderbackend-5rue.onrender.com/map/',
-  // API_URL_Ride: 'https://riderbackend-5rue.onrender.com/ride/',
+  SOCKET_URL: 'https://riderbackend-5rue.onrender.com/',
+  API_URL_USER: 'https://riderbackend-5rue.onrender.com/users/',
+  API_URL_CAPTAIN: 'https://riderbackend-5rue.onrender.com/captains/',
+  API_URL_MAP: 'https://riderbackend-5rue.onrender.com/map/',
+  API_URL_Ride: 'https://riderbackend-5rue.onrender.com/ride/',
 
-  SOCKET_URL: 'http://localhost:4000',
-  API_URL_USER: 'http://localhost:4000/users/',
-  API_URL_CAPTAIN: 'http://localhost:4000/captains/',
-  API_URL_MAP: 'http://localhost:4000/map/',
-  API_URL_Ride: 'http://localhost:4000/ride/',
-  API_URL_ADMIN: 'http://localhost:4000/admin/',
+  // SOCKET_URL: 'http://localhost:4000',
+  // API_URL_USER: 'http://localhost:4000/users/',
+  // API_URL_CAPTAIN: 'http://localhost:4000/captains/',
+  // API_URL_MAP: 'http://localhost:4000/map/',
+  // API_URL_Ride: 'http://localhost:4000/ride/',
+  // API_URL_ADMIN: 'http://localhost:4000/admin/',
 
   MAP_BOX_ACCESS_TOKEN: `pk.eyJ1IjoiZGV2ZW5kcmEtMTExIiwiYSI6ImNtNTVpN3A4ejJueWYyaXIwNGF1M25jcWkifQ.ZmwxLooOqfPUNL1hwbDeqQ`,
 
